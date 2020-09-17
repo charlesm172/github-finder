9869b92c8411a0904d6495d5aac48966d7599f60
Charles <charlesmorris172@gmail.com>
2020-09-17 17:37:30 +0100

    Added Search Bar

--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,5 +1,6 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 	return (
@@ -8,14 +9,14 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 				src={avatar_url}
 				alt='avatar'
 				className='round-img'
-				style={{ width: "60px" }}
+				style={{ width: '60px' }}
 			/>
 			<h3>{login}</h3>
 
 			<div>
-				<a href={html_url} className='btn btn-dark btn-sm my-1'>
+				<Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
 					More
-				</a>
+				</Link>
 			</div>
 		</div>
 	);
